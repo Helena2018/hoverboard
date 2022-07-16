2750296718e54bad9f242b4712b4cc579a4c787a
Helena2018 <helena2020liu@gmail.com>
2022-07-16 14:21:32 +1200

    set random color

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const container = document.getElementById('container')
-const color = ['#ee0979', '#ff6a00', '#DCE35B', '#45B649', '#FC466B', '#3F5EFB', '#40E0D0', '#FF8C00', '#FF0080', '#8A2387', '#E94057', '#FF0080']
+const colors  = ['#ee0979', '#ff6a00', '#DCE35B', '#45B649', '#FC466B', '#3F5EFB', '#40E0D0', '#FF8C00', '#FF0080', '#8A2387', '#E94057', '#FF0080']
 
 const SQUARE = 500
 
 for(let i = 0; i < SQUARE; i++) {
   const square = document.createElement('div')
   square.classList.add('square')
+
+  square.addEventListener('mouseenter', () => setColor(square))
+  square.addEventListener('mouseout', () => removeColor(square))
   container.appendChild(square)
+}
+
+function setColor(element) {
+  const color = getRandomColor()
+  element.style.background = color
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+
+}
+
+function removeColor(element) {
+  element.style.background = ''
+  element.style.boxShadow = ''
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)]
 }
